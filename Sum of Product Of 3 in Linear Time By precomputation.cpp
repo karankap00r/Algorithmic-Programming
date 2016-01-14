01ea89c15d004cc4c46f324a1219cd03e554da8e
Karan Kapoor <coffeewithkaran007@gmail.com>
2016-01-14 18:13:09 +0530

    Update Sum of Product Of 3 in Linear Time By precomputation.cpp

--- Sum of Product Of 3 in Linear Time By precomputation.cpp
+++ Sum of Product Of 3 in Linear Time By precomputation.cpp
@@ -1,7 +1,7 @@
 //Below program uses Precomputation to solve the problem in linear time 
 //Related Problem : https://www.hackerrank.com/challenges/kundu-and-tree
 
-long long int sum = 0;      //Final Answer
+long long int sum = 0;      
 B[n-1] = ct[n];
 for(i=n-2;i>=0;i--) B[i] = (B[i+1] + ct[i+1])%MOD;
 for(i=1;i<n-1;i++) C[i] = (ct[i+1]*B[i+1])%MOD;
@@ -10,4 +10,4 @@ D[n-2] = C[n-2];
 for(i=n-3;i>=1;i--) D[i] = (D[i+1] + C[i])%MOD;
 
 for(i=0;i<n-2;i++) sum = (sum + ct[i+1]*D[i+1])%MOD;
-cout<< ( MOD + sum )%MOD<<endl;
+cout<< ( MOD + sum )%MOD<<endl;   //Final Answer
